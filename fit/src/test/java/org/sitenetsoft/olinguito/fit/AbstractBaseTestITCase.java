e80cb0931491dd487852e699a596c5d09fd4d8fd
jnbdz <237008+jnbdz@users.noreply.github.com>
2026-02-02 20:49:37 -0500

    Got all the test to run

--- fit/src/test/java/org/sitenetsoft/olinguito/fit/AbstractBaseTestITCase.java
+++ fit/src/test/java/org/sitenetsoft/olinguito/fit/AbstractBaseTestITCase.java
@@ -48,7 +48,7 @@ public abstract class AbstractBaseTestITCase {
     server = TomcatTestServer.init(9080)
         .addServlet(TechnicalServlet.class, "/odata-server-tecsvc/odata.svc/*")
         .addServlet(TechnicalStatusMonitorServlet.class, "/odata-server-tecsvc/status/*")
-        //.addAuthServlet(TechnicalServlet.class, "/odata-server-tecsvc/auth", "/*")
+        .addAuthServlet(TechnicalServlet.class, "/odata-server-tecsvc/auth", "/*")
         .addServlet(StaticContent.create("org-odata-core-v1.xml"),
             "/odata-server-tecsvc/v4.0/cs02/vocabularies/Org.OData.Core.V1.xml")
         .addWebApp(false)
