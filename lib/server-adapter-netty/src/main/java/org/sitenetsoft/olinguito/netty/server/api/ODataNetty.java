9ea1ef837351f61255a280a464a23aa9c8e3d45d
jnbdz <237008+jnbdz@users.noreply.github.com>
2026-02-09 22:56:08 -0500

    Extract server-adapter-netty module to decouple Netty from server-api and server-core
       Apply the same adapter pattern used for servlets: move ODataNetty, ODataNettyHandler,
       ODataNettyHandlerImpl, and ODataNettyImpl into a new server-adapter-netty module,
       removing the io.netty:netty-codec-http dependency from server-api and server-core.
       Package names are preserved to avoid breaking downstream imports.

--- lib/server-adapter-netty/src/main/java/org/sitenetsoft/olinguito/netty/server/api/ODataNetty.java
+++ lib/server-adapter-netty/src/main/java/org/sitenetsoft/olinguito/netty/server/api/ODataNetty.java
@@ -15,6 +15,8 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ * Copyright 2026 SiteNetSoft - Moved to server-adapter-netty module
  */
 package org.sitenetsoft.olinguito.netty.server.api;
 
